facfe03ff8e2e4ff267ce10f7c75571777deb99d
Ondolin <developer@marvin-kaiser.eu>
2021-08-03 01:03:36 +0200

    added error for bad config file

--- config/readConfig.go
+++ config/readConfig.go
@@ -38,3 +38,39 @@ func readConf() *Config {
 }
 
 var Configuration = readConf()
+
+func checkValues(config *Config) {
+
+	if config.Gotify.URL == "" {
+		log.Fatal("No gotify url specified.")
+	}
+
+	if config.Gotify.ApiToken == "" {
+		log.Fatal("No gotify api token specified.")
+	}
+
+	if config.Gotify.PollTime == "" {
+		log.Fatal("No gotify polltime specified.")
+	}
+
+	if config.Matrix.HomeServerURL == "" {
+		log.Fatal("No matrix homeserver specified.")
+	}
+
+	if config.Matrix.Username == "" {
+		log.Fatal("No matrix username specified.")
+	}
+
+	if config.Matrix.Token == "" {
+		log.Fatal("No matrix auth token specified.")
+	}
+
+	if config.Matrix.RoomID == "" {
+		log.Fatal("No matrix room id specified.")
+	}
+
+	if !config.Matrix.Encrypted {
+		log.Fatal("No encryption specified. Please use true or false")
+	}
+
+}
